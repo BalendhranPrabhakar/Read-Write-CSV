b6d0de939b8d5a3c09a3b61ccaee55838ce34d9a
BalendhranPrabhakar <114464251+BalendhranPrabhakar@users.noreply.github.com>
2024-07-31 12:06:05 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+class Employee
+{
+    [Name("First Name")]
+    public string? FirstName { get; set; }
+
+    [Name("Last Name")]
+    public string? LastName { get; set; }
+
+    [Name("Joined Date")]
+    public DateOnly? JoinedDate { get; set; }
+
+    [Name("Salary")]
+    public decimal? Salary { get; set; }
+
+    [Name("Active")]
+    public string? Active { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var employees = new List<Employee> 
+        {
+   new Employee 
+   {
+      FirstName = "John", LastName = "Doe", JoinedDate = new DateOnly(2020, 1, 1), Salary = 50000.00M, Active = "Yes"
+   },
+   new Employee 
+   {
+      FirstName = "Jane", LastName = "Doe", JoinedDate = new DateOnly(2021, 6, 15), Salary = 60000.00M, Active = "Yes"
+   },
+   new Employee 
+   {
+      FirstName = "Bob", LastName = "Smith", JoinedDate = new DateOnly(2019, 3, 10), Salary = 70000.00M, Active = "No"
+   }
+};
+
+
+        using var writer = new StreamWriter("output.csv");
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+
+        csv.WriteHeader<Employee>();
+        csv.NextRecord();
+        foreach (var employee in employees)
+        {
+            csv.WriteRecord(employee);
+            csv.NextRecord();
+        }
+
+    }
+}
